87fde5430e72b5bc551d2899a6d7dd2a1beb8f66
cakeller2 <cakeller2@quinnipiac.edu>
2024-03-03 17:29:24 -0500

    range updated

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -7,7 +7,7 @@ public class DmvSimulator {
         System.out.println("Welcome to the DMV! Please get in line and grab a number");
 
         Random random = new Random();
-        int ran = random.nextInt(101 - 1) + 1;
+        int ran = random.nextInt(201 - 100) + 100;
 
         System.out.println("Your number is " + ran + ".\nPlease go sit down and wait until your number is called");
 
