4a9aa96b88f25d0448dc795d58d431ad7e045556
cakeller2 <cakeller2@quinnipiac.edu>
2024-03-03 16:08:44 -0500

    step one intro message

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -2,7 +2,6 @@ package DmvSimulator;
 
 public class DmvSimulator {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Test part 2");
+        System.out.println("Welcome to the DMV! Please get in line and grab a number");
     }
 }
