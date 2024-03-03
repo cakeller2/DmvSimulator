d93a12bf4a82df30b0e3a2e1fee8f2f30cb42c59
cakeller2 <cakeller2@quinnipiac.edu>
2024-03-03 16:27:34 -0500

    random number step 2

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -1,7 +1,14 @@
 package DmvSimulator;
 
+import java.util.Random;
+
 public class DmvSimulator {
     public static void main(String[] args) {
         System.out.println("Welcome to the DMV! Please get in line and grab a number");
+
+        Random random = new Random();
+        int ran = random.nextInt(101 - 1) + 1;
+
+        System.out.println("Your number is " + ran + ".\nPlease go sit down and wait until your number is called");
     }
 }
