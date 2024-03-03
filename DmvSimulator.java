67e107b65360a6be70f91de7e6cf0c05458b2939
cakeller2 <cakeller2@quinnipiac.edu>
2024-03-03 17:13:56 -0500

    number loop step 3

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -10,5 +10,20 @@ public class DmvSimulator {
         int ran = random.nextInt(101 - 1) + 1;
 
         System.out.println("Your number is " + ran + ".\nPlease go sit down and wait until your number is called");
-    }
+
+        int number = ran; 
+
+        for (int i = 1; (ran + i) != number; i++) {
+            if ((ran + i) > 100) {
+                ran = 0;
+                i = 1;
+            }
+            System.out.println((ran + i) + " is up next. Please come to the counter");
+        }
+
+        System.out.println(number + " is up next. (That's you!) Please come to the counter");
+    }   
 }
+
+/*Call out each number one at a time, but be sure to start from the number directly after the one
+you gave to the user and then wrap around until the user’s number is finally called last*/
