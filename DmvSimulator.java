efa1488ad5f205b7d3892ee2b53d133feb9c12b9
cakeller2 <cakeller2@quinnipiac.edu>
2024-03-03 15:54:00 -0500

    test part 2

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -3,5 +3,6 @@ package DmvSimulator;
 public class DmvSimulator {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Test part 2");
     }
 }
